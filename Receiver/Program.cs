a0b05337d152f4cfadcf8446f05783c142a1ff93
brunoluizdesiqueira <brunoluizdesiqueira@gmail.com>
2018-07-08 23:29:28 -0300

    Commit inicial

---
+++ Receiver/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Amqp;
+
+//using Amqp.Framing;
+//using Amqp.Listener;
+//using Amqp.Types;
+
+namespace Receiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address("amqp://guest:guest@localhost:5672");
+            Connection connection = new Connection(address);
+            Session session = new Session(connection);
+            ReceiverLink receiver = new ReceiverLink(session, "receiver-link", "q1");
+
+            Console.WriteLine("Receiver connected to broker.");
+         
+            while (true)
+            { 
+                Message response = receiver.Receive(TimeSpan.FromMilliseconds(2000));       
+            
+                if (response != null)
+                {
+                    receiver.Accept(response);
+                    Console.WriteLine("Received response: {0} body {1}", response.Properties, response.Body);
+                
+                    if (string.Equals("done", response.Body))
+                    {
+                        break;
+                    } 
+                }                
+            }
+
+            receiver.Close();
+            session.Close();
+            connection.Close();
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Amqp;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address("amqp://guest:guest@localhost:5672");
+            Connection connection = new Connection(address);
+            Session session = new Session(connection);
+
+            Message message = new Message("Outra mensagem enviada para AMQP");
+            SenderLink sender = new SenderLink(session, "sender-link", "q1");
+            sender.Send(message);
+            Console.WriteLine("Sent Hello AMQP!");
+
+            sender.Close();
+            session.Close();
+            connection.Close();
+        }
+    }
+}
